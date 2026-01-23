56f82d0ce87334bf6a61c621bd9a073014718514
Valery-AA <valeryatart@gmail.com>
2026-01-23 22:27:54 +0100

    Initial Setup

--- Warudo-Tools/__init__.py
+++ Warudo-Tools/__init__.py
@@ -1,8 +1,3 @@
-import importlib
-from .AlxModuleManager import (
-    Alx_Module_Manager
-)
-
 import bpy
 
 
@@ -20,19 +15,15 @@ bl_info = {
 }
 
 
-module_loader = Alx_Module_Manager(__path__, globals())
 
 
 def register():
-    module_loader.developer_register_modules()
-    module_loader.developer_process_module_keymaps()
 
     bpy.context.preferences.use_preferences_save = True
 
 
 def unregister():
-    module_loader.developer_unregister_modules()
-
+    pass
 
 if __name__ == "__main__":
     register()
